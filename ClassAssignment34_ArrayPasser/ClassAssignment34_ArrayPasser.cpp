1c0f306674c862aa1b081322ec5d1b9bd8dd849d
TrevSh <trevonshelby@outlook.com>
2023-07-28 21:59:17 -0500

    Add project files.

---
+++ ClassAssignment34_ArrayPasser/ClassAssignment34_ArrayPasser.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+void increase(int* const array, const int NUM_ELEMENTS);
+void display(const int* const array, const int NUM_ELEMENTS);
+
+int main()
+{
+    std::cout << "Creating an array of high scores. \n\n";
+    const int NUM_SCORES = 3;
+    int highScores[NUM_SCORES] = { 5000,3500,2700 };
+
+    std::cout << "Displaying scores using array name as a constant pointer. \n";
+    std::cout << *highScores << "\n";
+    std::cout << *(highScores +1) << "\n";
+    std::cout << *(highScores +2) << "\n\n";
+
+    std::cout << "Increasing scores by passing array as a constant pointer.\n\n";
+    increase(highScores, NUM_SCORES);
+
+    std::cout << "Displaying scores by passing array as a constant pointer to a constant. \n";
+    display(highScores, NUM_SCORES);
+
+    system("pause");
+}
+
+void increase(int* const array, const int NUM_ELEMENTS) {
+    for (int i = 0; i < NUM_ELEMENTS; ++i) {
+        array[i] += 500;
+    }
+}
+
+void display(const int* const array, const int NUM_ELEMENTS) {
+    for (int i = 0; i < NUM_ELEMENTS; ++i) {
+        std::cout << array[i] << "\n";
+    }
+}
